d78f05c7ca0060af86846e957c013d8ab472fc61
garyxiong123 <502238410@qq.com>
2023-01-25 02:21:18 +0800

    init

--- P/P_test.go
+++ P/P_test.go
@@ -1,7 +1,15 @@
 package P
 
-import "testing"
+import (
+	"github.com/garyxiong123/go-depency-B/B"
+)
 
-func TestDiffrentTag(t *testing.T) {
-
+func Go_dependency_B_V1() {
+	B.Go_dependency_B_V1()
+	println("go_dependency_B_V1")
 }
+
+//
+//func TestDiffrentTag(t *testing.T) {
+//	Go_dependency_B_V1()
+//}
